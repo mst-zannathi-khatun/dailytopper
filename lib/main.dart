1e17c689a5ace99cbfe32c1a37f0a68581a6d41c
zannathi-khatun <m.zannathi.k@gmail.com>
2024-10-08 10:13:16 +0530

    Importing GetX for state management, dependency injection, and route management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:dailytopper/ui/screens/bottom_navigationbar.dart';
+import 'package:dailytopper/ui/state_manager/bottom_navigationbar_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 
-void main(){
+void main() {
   runApp(const DailyTopperApp());
 }
 
@@ -10,8 +13,15 @@ class DailyTopperApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: BottomNavigationBarScreen(),
-    );
+    return GetMaterialApp(
+      initialBinding: GetXBindings(),
+        home: const BottomNavigationBarScreen(),);
+  }
+}
+
+class GetXBindings extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(BottomNavigationBarController());
   }
 }
